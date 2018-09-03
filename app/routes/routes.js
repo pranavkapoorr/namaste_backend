7100e4b923eb2d98a4ae7724b64d80f831970290
Pranav Kapoor <pranavkapoorr@gmail.com>
2018-09-03 10:13:47 -0400

    first push

---
+++ app/routes/routes.js
@@ -0,0 +1,53 @@
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
+    //create
+    app.post('/user', (req, res) => {
+        const user =  req.body;
+        db.collection('users').insert(user, (err, result) => {
+            if (err) { 
+                console.log(err);
+                res.send({ 'error': 'An error has occurred' }); 
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
+    //retreive
+    app.get('/users/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('users').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                    res.send(item!=null?item:"User doesn't exist!");
+            }
+        });
+    });
+    //update
+    app.put('/users/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        const user = req.body;
+        db.collection('users').update(details, user, (err, result) => {
+          if (err) {
+              res.send({'error':'An error has occurred'});
+          } else {
+              res.send(user);
+          } 
+        });
+      });
+      //delete
+      app.delete('/users/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('users').remove(details, (err, item) => {
+          if (err) {
+            res.send({'error':'An error has occurred'});
+          } else {
+            res.send('user ' + id + ' deleted!');
+          } 
+        });
+      });
+};
---
+++ namaste.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const db             = require('./config/db');
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+    require('./app/routes')(app, database);
+    app.listen(port, () => {console.log('Server started and listening on ' + port)});
+  });
